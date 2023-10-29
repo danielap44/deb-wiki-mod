24608d882fb5322510cd6f4521bcbb9610024409
Daniel Adepitan <adepitandaniel44@gmail.com>
2023-10-29 11:55:58 +0100

    update: improve command line API

--- deb_wiki_mod/lib/cli.py
+++ deb_wiki_mod/lib/cli.py
@@ -1,14 +1,19 @@
 from argparse import ArgumentParser
-from dataclasses import dataclass
-from os.path import basename, abspath
+from dataclasses import dataclass, field
 from typing import Optional
 
+__all__ = ("get_cli_options",)
 
-_parser = ArgumentParser()
+
+_parser = ArgumentParser(
+    prog="DebianWikiModernizer",
+    description="A basic Python package to convert HTML pages to markdown",
+)
 _parser.add_argument(
-    "url",
+    "urls",
     type=str,
-    help="The url to the page to parse and convert to markdown",
+    nargs="+",
+    help="A list of page urls to parse and convert to markdown",
 )
 _parser.add_argument(
     "--main",
@@ -17,29 +22,47 @@ _parser.add_argument(
     help="The ID of the main content area of the page",
 )
 _parser.add_argument(
-    "-o",
-    "--output",
+    "-f",
+    "--out-file",
     type=str,
     required=False,
-    help="The output filename for the resulting markdown",
+    help="The output filename for the resulting markdown.",
+)
+_parser.add_argument(
+    "-d",
+    "--out-dir",
+    type=str,
+    required=False,
+    help="The output dirname for the resulting markdown.",
 )
 
 
 @dataclass(kw_only=True)
 class CLIOptions:
-    url: str = ...  # type: ignore
+    urls: list[str] = field(default_factory=list)
     main: Optional[str] = None
-    output: str = ...  # type: ignore
+    out_file: Optional[str] = None
+    out_dir: Optional[str] = None
 
 
-def get_cli_options():
-    args = _parser.parse_args()
+def get_cli_options(argv: list[str]):
+    """Given a list of command line arguments, parse the argument and serialize them into a dataclass object
+
+    Args:
+        argv: The list of command line arguments
+    Returns:
+        options: The CLIOptions dataclass object into which the argments are serialized
+    Raises:
+        Exception: When --out-file is specified with multiple urls (e.g, program url1 url2 --out-file path)
+    """
+    args = _parser.parse_args(argv)
     options = CLIOptions()
     attrs = options.__dict__.keys()
 
     for attr in attrs:
         setattr(options, attr, getattr(args, attr))
-
-    if not options.output:
-        options.output = abspath(basename(options.url) + ".md")
+    if len(options.urls) > 1 and options.out_file is not None:
+        raise Exception(
+            "--out-file cannot be specified with multiple urls (consider --out-dir)"
+        )
     return options
