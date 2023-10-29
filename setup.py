529cd3a5e21d53277a0168bea01496358675148d
Daniel Adepitan <adepitandaniel44@gmail.com>
2023-10-29 20:01:58 +0100

    Fix typo in project description

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     name="deb_wiki_mod",
     packages=find_packages(exclude=["tests"]),
     version="0.1.0",
-    description="A simple to to convert a HTML page to Markdown",
+    description="A simple Python command line program to convert any HTML page to Markdown",
     author="Daniel Adepitan <danieladepitan44@gmail.com>",
     author_email="<danieladepitan44@gmail.com>",
-    keywords=["debian", "wiki", "markdown"],
+    keywords=["debian", "wiki", "html", "markdown"],
     python_requires=">=3.5",
     install_requires=get_requirements(),
     entry_points={
