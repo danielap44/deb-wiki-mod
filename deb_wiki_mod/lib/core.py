fafc1672a666188cb5ebe3be51020ac2e4c4e9f2
Daniel Adepitan <adepitandaniel44@gmail.com>
2023-10-29 11:56:53 +0100

    Add docstrings to functions

--- deb_wiki_mod/lib/core.py
+++ deb_wiki_mod/lib/core.py
@@ -1,3 +1,4 @@
+from os.path import basename, abspath, join
 from typing import Optional
 
 import requests
@@ -5,20 +6,38 @@ import html2text
 from bs4 import BeautifulSoup
 
 __all__ = (
+    "compute_output_file",
     "convert_to_makrdown",
     "fetch_debian_news_page",
-    "resolve_content",
     "html2text_factory",
+    "plural",
+    "resolve_content",
     "write_markdown_to_output_file",
 )
 
 
 def fetch_debian_news_page(url: str):
+    """Fetches the page by URL using the requests library.
+
+    Args:
+        url: The string url of the page to fetch
+    Returns:
+        response: The response object from the request
+    """
     response = requests.get(url)
+    response.raise_for_status()
     return response
 
 
 def resolve_content(html: str, *, content_id: Optional[str]):
+    """Resolves the main content area of an HTML string after parsing with BeautifulSoup.
+
+    Args:
+        html: The HTML string to parse and resolve main content from
+        content_id: The id of the main page element in the HTML string
+    Returns:
+        content: The resolved main content element in the HTML string
+    """
     soup = BeautifulSoup(html, "html.parser")
     if content_id is None:
         return soup
@@ -34,6 +53,13 @@ def resolve_content(html: str, *, content_id: Optional[str]):
 
 
 def html2text_factory(baseurl: str):
+    """A factory function for constructing a HTML2Text object and configuring specific attributes.
+
+    Args:
+        baseurl: The baseurl or the origin url to resolve all relative links to
+    Returns:
+        h: The HTML2Text object
+    """
     h = html2text.HTML2Text(baseurl=baseurl)
     h.inline_links = False
     h.wrap_links = False
@@ -42,9 +68,56 @@ def html2text_factory(baseurl: str):
 
 
 def convert_to_makrdown(converter: html2text.HTML2Text, html: str):
+    """Converts an HTML string to markdown using a converter supplied as one of the args.
+
+    Args:
+        converter: An instance of HTML2Text to use to convert the HTML string to markdown.
+        html: The HTML string to convert.
+    Returns:
+        markdown: The converted markdown string
+    """
     return converter.handle(html)
 
 
 def write_markdown_to_output_file(*, markdown: str, filename: str):
+    """Writes a markdown string to an output file with a given filename
+
+    Args:
+        markdown: The markdown string to write to a file.
+        filename: The filename or file path to write the markdown file to.
+    Returns:
+        None
+    """
     with open(filename, "w", encoding="utf-8") as file:
         file.write(markdown)
+
+
+def compute_output_file(*, url: str, outfile: Optional[str], outdir: Optional[str]):
+    """Compute the resultant output filename or file path from a set of variable options.
+
+    Args:
+        url: The url of the page whose markdown content is being written to a file
+        outfile: The out-file arg supplied from the command line read from CLIOptions.
+        ourdir: The out-dir arg supplied from the command line read from CLIOptions.
+    Returs:
+        The eventual file path taking into account all variables.
+    """
+    filename = basename(url) + ".md"
+    if outfile:
+        return outfile
+    if not outdir:
+        return abspath(filename)
+    return abspath(join(outdir, filename))
+
+
+def plural(count: int, singular: str, plural: str):
+    """Return plural if count is greater than 1, otherwise return singular.
+
+    Args:
+        count: The number of an arbitrary item
+        singular: The singular word variant or the default
+        plural: The plural word variant
+    Returns:
+        returns signular or plural
+    """
+    return plural if count > 1 else singular
